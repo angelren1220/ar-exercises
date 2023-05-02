a900bd72533678d96805a56920342b55c25d7f7f
Linuxe <angelren1220@gmail.com>
2023-05-02 16:40:20 -0400

    exercise3 and exercise4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,25 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+store1 = Store.create(
+  name: "Surrey",
+  annual_revenue: 224000,
+  mens_apparel: true,
+  womens_apparel: false
+)
+
+store2 = Store.create(
+  name: "Whistler",
+  annual_revenue: 1900000,
+  mens_apparel: true,
+  womens_apparel: false
+)
+
+store3 = Store.create(
+  name: "Yaletown",
+  annual_revenue: 430000,
+  mens_apparel: true,
+  womens_apparel: true
+)
+
+puts Store.count
